fe9e1a833dfb562a364492ca691a64b1aebf5b72
rocaruler <vladnovoren@gmail.com>
2024-07-12 22:16:35 +0300

    dist

--- src/war_utils.py
+++ src/war_utils.py
@@ -1,6 +1,16 @@
 from api import get_dynamic_objects
+from math import sqrt
 from models import Location
 
-def can_attack(loc: Location):
-    bases_coords = [Location(base['x'], base['y']) for base in get_dynamic_objects()['base']]
-    dist <= sqrt(abs(x1-x2)^2 + abs(y1-y2)^2)
+def dist_sqr(p1: Location, p2: Location):
+    return (p1-p2)**2 + (p1-p2)**2
+
+def dist(p1: Location, p2: Location):
+    return sqrt(dist_sqr(p1, p2))
+
+def can_attack(bases, loc: Location):
+    result = []
+    for base in bases:
+        if dist_sqr(loc, base.location) <= base.range:
+            result.append(base)
+    return result
