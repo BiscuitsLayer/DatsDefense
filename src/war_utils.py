faffb4af957345050e6c31291691e0df3cdc535d
rocaruler <vladnovoren@gmail.com>
2024-07-12 23:26:48 +0300

    haha

--- src/war_utils.py
+++ src/war_utils.py
@@ -1,16 +1,24 @@
 from api import get_dynamic_objects
 from math import sqrt
-from models import Location
+from typing import List
+from models import Location, Base, Zombie
 
 def dist_sqr(p1: Location, p2: Location):
-    return (p1-p2)**2 + (p1-p2)**2
+    return (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2
 
 def dist(p1: Location, p2: Location):
     return sqrt(dist_sqr(p1, p2))
 
-def can_attack(bases, loc: Location):
+def can_attack(bases: List[Base], loc: Location) -> List[str]:
     result = []
     for base in bases:
-        if dist_sqr(loc, base.location) <= base.range:
-            result.append(base)
+        if dist_sqr(loc, Location(base.x, base.y)) <= base.range ** 2:
+            result.append(base.id)
     return result
+
+def rank_zombies(zombies: List[Zombie]) -> List[Zombie]:
+    result = []
+    
+
+def zombie_order() -> List[str]:
+    pass
