41be0dd089ee1c8612a96260453e85de19c1bf4d
BiscuitsLayer <vinogradov.so@phystech.edu>
2024-07-13 15:17:50 +0300

    Autoregister, more logic

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import os
+import time
 from dotenv import load_dotenv
 
-from src.bfs import build_route
+from src.bfs import build_route, can_build_here
 from src.build_utils import attack_enemies, build_route_to_enemies
+from src.utils import get_neighbor
 load_dotenv()
 
 from src.api import *
 from src.const import ITER_TIME
 from src.planner import IntervalRunner, Planner
-from src.models import Build, Vec2, Attack
+from src.models import *
 
 def main():
     # Verify token and register for a round
     print(f"Token is {os.getenv('TOKEN')}")
     msg, participating = participate()
     print(msg)
-    if not participating:
-        print("Exiting")
-        return None
+
+    while not participating:
+        msg, participating = participate()
+        print(msg)
+        time.sleep(2)
+        # print("Exiting")
+        # return None
     
     # Create context to avoid doing many requests for a single iteration
     context = Context()
@@ -34,20 +40,27 @@ def main():
             ###
             # ADD ALL LOGIC HERE
             ###
+
             map = map(context.bases, context.enemy_bases, context.zombies, context.zpots)
 
-            routes = build_route_to_enemies(
+            routes_to_enemies = build_route_to_enemies(
                 n_to_build=5,
                 enemy_bases_coords=[Vec2(x=base.x, y=base.y) for base in context.enemy_bases],
                 bases_coords=[Vec2(x=base.x, y=base.y) for base in context.bases],
                 zombies=context.zombies,
                 map=map
             )
-            if routes:
-                best_route = routes[0]
+            if routes_to_enemies:
+                best_route = routes_to_enemies[0]
                 for loc in best_route:
                     build = Build(x=loc.x+1, y=loc.y+1)
                     planner.plan_build(build)
+            else:
+                for base in context.bases:
+                    for loc in (get_neighbor(Vec2(x=base.x, y=base.y), neighbor_type) for neighbor_type in NeighborType):
+                        if can_build_here(loc, map, context.zombies):
+                            build = Build(x=loc.x, y=loc.y)
+                            planner.plan_build(build)
 
             attacks = attack_enemies(
                 n_to_attack=5,
@@ -62,7 +75,6 @@ def main():
             if attacks:
                 for attack in attacks:
                     planner.plan_attack(attack)
-
                 
         except KeyboardInterrupt:
             print("Shutting down...")
